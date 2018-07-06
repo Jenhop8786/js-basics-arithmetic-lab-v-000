ced4cd87ac9d765252c90c12ccceabc7117c118e
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-07-06 18:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ const newID = oldID + 1000000000;
 const ageIsValid = Number.isInteger(currentAge);
 
 const randomNumber = (Math.random() * 20 );
+
+const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = randomInteger + 1000000001; 
